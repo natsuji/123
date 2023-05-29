dc2e41db52b6b5267cb9e59ba35b0b89b4a05ade
natsuji <130963552+natsuji@users.noreply.github.com>
2023-05-29 15:07:03 +0800

    Update script.js

--- 111/dist/script.js
+++ 111/dist/script.js
@@ -1,1 +1,10 @@
 document.write("日安安安安 這裡是許雅茵desu")
+
+
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count+=2;
+      counterElement.textContent = count;
+    }
